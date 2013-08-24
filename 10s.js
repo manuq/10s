67c15093677ace33da3a40f2aea6cb74fd514fb7
Manuel Quiñones <manuq@laptop.org>
2013-08-24 12:07:03 -0300

    easeljs

--- 10s.js
+++ 10s.js
@@ -1,2 +1,10 @@
-alert("hello world");
+var stage = new createjs.Stage("canvas");
+
+var circle = new createjs.Shape();
+circle.graphics.beginFill("#555").drawCircle(0, 0, 50);
+circle.x = 100;
+circle.y = 100;
+stage.addChild(circle);
+
+stage.update();
 //# sourceMappingURL=10s.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-alert("hello world")
+declare var createjs;
+
+var stage = new createjs.Stage("canvas");
+
+var circle = new createjs.Shape();
+circle.graphics.beginFill("#555").drawCircle(0, 0, 50);
+circle.x = 100;
+circle.y = 100;
+stage.addChild(circle);
+
+stage.update();
